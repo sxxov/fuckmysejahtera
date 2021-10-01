87ab09c81bc7e885af8461adc6c2ae4720de9737
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-10-01 22:56:44 +0800

    Move responsive appbar setup to `Activity`

--- app/src/main/java/design/sxxov/fuckmysejahtera/blocks/classes/Activity.java
+++ app/src/main/java/design/sxxov/fuckmysejahtera/blocks/classes/Activity.java
@@ -2,24 +2,71 @@ package design.sxxov.fuckmysejahtera.blocks.classes;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ScrollView;
 
 import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import design.sxxov.fuckmysejahtera.R;
 import design.sxxov.fuckmysejahtera.settings.SettingsItem;
 import design.sxxov.fuckmysejahtera.settings.SettingsStorage;
 import design.sxxov.fuckmysejahtera.utilities.ResourceUtility;
 import design.sxxov.fuckmysejahtera.utilities.SnackbarUtility;
+import design.sxxov.fuckmysejahtera.utilities.WindowUtility;
 
 public class Activity extends AppCompatActivity {
+    private final AtomicBoolean isAppbarLayoutScheduled = new AtomicBoolean();
+    protected WindowUtility windowUtility;
     protected SnackbarUtility snackbarUtility;
     protected ResourceUtility resourceUtility;
     protected SettingsStorage settingsStorage;
     protected SettingsItem state;
     protected int thisLayoutResId;
+    protected int appbarResId;
+    protected int scrollableViewResId;
+    private View appbar;
+    private View scrollableView;
+    private int initialAppbarHeight;
+
+    public View getAppbar() {
+        return this.appbar;
+    }
+
+    public int getInitialAppbarHeight() {
+        return this.initialAppbarHeight;
+    }
+
+    protected void setInitialAppbarHeight(int initialAppbarHeight) {
+        this.appbar.getLayoutParams().height = initialAppbarHeight;
+
+        if (this.isAppbarLayoutScheduled.get())
+            return;
+
+        this.isAppbarLayoutScheduled.set(true);
+
+        this.appbar.postOnAnimation(() -> {
+            if (!this.isAppbarLayoutScheduled.get())
+                return;
+
+            this.isAppbarLayoutScheduled.set(false);
+            this.appbar.requestLayout();
+        });
+    }
+
+    protected void setScrollableViewResId(int scrollableViewResId) {
+        this.scrollableViewResId = scrollableViewResId;
+    }
+
+    protected void setAppbarViewResId(int appbarViewResId) {
+        this.appbarResId = appbarViewResId;
+    }
 
     protected void setContentViewResId(int thisLayoutResId) {
         this.thisLayoutResId = thisLayoutResId;
@@ -34,8 +81,29 @@ public class Activity extends AppCompatActivity {
 
         this.setupTheme();
 
-        if (this.thisLayoutResId != -1) {
+        if (this.thisLayoutResId > 0) {
             this.setContentView(this.thisLayoutResId);
+        }
+
+        this.windowUtility = new WindowUtility(this);
+
+        if (this.appbarResId > 0 && this.scrollableViewResId > 0) {
+            this.appbar = this.findViewById(this.appbarResId);
+            this.scrollableView = this.findViewById(this.scrollableViewResId);
+
+            final ViewTreeObserver observer = this.appbar.getViewTreeObserver();
+            observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @Override
+                public void onGlobalLayout() {
+                    Activity.this.initialAppbarHeight = Activity.this.appbar.getHeight();
+
+                    Activity.this.setupResponsiveAppbar();
+
+                    Activity.this.appbar
+                            .getViewTreeObserver()
+                            .removeOnGlobalLayoutListener(this);
+                }
+            });
         }
 
         this.snackbarUtility = new SnackbarUtility(this);
@@ -43,6 +111,46 @@ public class Activity extends AppCompatActivity {
 
         this.setupStatusBar();
         this.setupNavigationBar();
+    }
+
+    private void setupResponsiveAppbar() {
+        View scrollableView = this.findViewById(this.scrollableViewResId);
+
+        if (scrollableView instanceof ScrollView) {
+            scrollableView.getViewTreeObserver().addOnScrollChangedListener(
+                    () -> this.setInitialAppbarHeight(
+                            Math.max(
+                                    this.initialAppbarHeight - this.scrollableView.getScrollY(),
+                                    (int) (this.initialAppbarHeight / 2f)
+                            )
+                    ));
+
+            return;
+        }
+
+        if (scrollableView instanceof RecyclerView) {
+            ((RecyclerView) scrollableView)
+                    .addOnScrollListener(new RecyclerView.OnScrollListener() {
+                        @Override
+                        public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                            super.onScrolled(recyclerView, dx, dy);
+
+                            Activity.this.setInitialAppbarHeight(
+                                    Math.max(
+                                            initialAppbarHeight
+                                                    - recyclerView.computeVerticalScrollOffset(),
+                                            (int) (initialAppbarHeight / 2f)
+                                    )
+                            );
+                        }
+                    });
+
+            return;
+        }
+
+        throw new IllegalStateException(
+                "Activity#scrollableView is not instance of either ScrollView or RecyclerView"
+        );
     }
 
     private void setupTheme() {
